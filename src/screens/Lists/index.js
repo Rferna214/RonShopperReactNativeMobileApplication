f29314f8161da38e8b6a0fad5780757b20ae0f58
Ronald Fernandez <Rferna214@gmail.com>
2022-09-07 10:44:01 -0400

    September 7, 2022

--- src/screens/Lists/index.js
+++ src/screens/Lists/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const ListsScreen = props => {
+
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
         <View style={styles.bottom}>
             <TouchableOpacity 
             style={styles.button}
-            onPress={() => console.log('Add List!')}
+            onPress={() => navigation.navigate('Add List')}
             >
                 <Text style={styles.buttonText}>Add List</Text>
             </TouchableOpacity>
