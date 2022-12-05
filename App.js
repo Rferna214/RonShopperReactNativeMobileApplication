506eacbb421d8cd85f7394b45a07785e80804dfc
Ronald Fernandez <Rferna214@gmail.com>
2022-12-05 14:09:36 -0500

    Setting up login

--- App.js
+++ App.js
@@ -10,9 +10,26 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import type {Node} from 'react';
 import Router from './src/navigation/Router';
-
+// bcrypt is a secure way to save passwords in a database
+// its algorithms encrypt a password into a long string of
+// characters, called a hash, that is almost impossible to
+// decrypt
+// it makes a database more secure - if someone hack into
+// it, he won't be able to steal the user's passwords
+import bcrypt from 'react-native-bcrypt';
+// Import openDatabase hook
+import { openDatabase } from "react-native-sqlite-storage";
 
 const database = require('./src/components/Handlers/database.js');
+
+// use hook to create database
+const shopperDB = openDatabase({name: 'Shopper.db'});
+const usersTableName = 'user';
+
+// create a salt that will be used by bcrypt when creating the hash
+// a salt is a random value that will be appended to the password
+// before it's encrypted
+let salt = bcrypt.genSaltSync(10);
 
 const App: () => Node = () => {
   try {
@@ -29,6 +46,20 @@ const App: () => Node = () => {
     database.createListsItemsTable();
   } catch (error) {
     console.log('Failed to create List items table '+ error);
+  }
+  try {
+    database.creatUsersTable();
+  } catch (error) {
+    console.log('Failed to create Users table '+ error);
+  }
+
+  try {
+    //create the hash
+    let hash = bcrypt.hashSync('password', salt);
+    // database.addUser('Ronald', hash);
+    database.creatUsersTable();
+  } catch (error) {
+    console.log('Failed to create User '+ error);
   }
   return <Router />;
 };
